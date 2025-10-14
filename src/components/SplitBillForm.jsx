f232667da91f300436d9ecb113430f69ca2472a8
adam <adamalhafizh23@gmail.com>
2025-10-14 13:58:25 +0700

    Update Progress

--- src/components/SplitBillForm.jsx
+++ src/components/SplitBillForm.jsx
@@ -27,13 +27,18 @@ export default function SplitBillForm({ onClose, transaction }) {
     <div className="splitbill-modal">
       <div className="splitbill-container">
         <h2 className="title">Split Bill Form</h2>
+        <hr />
         <div className="transaction-info">
-          <p>
-            <strong>Transaction ID :</strong> {transaction.id}
-          </p>
-          <p>
-            <strong>Transaction Date :</strong> {transaction.date}
-          </p>
+          <div className="transaction-info-left-panel">
+            <p>
+              <strong>Transaction ID :</strong> {transaction.id}
+            </p>
+          </div>
+          <div className="transaction-info-right-panel">
+            <p>
+              <strong>Transaction Date :</strong> {transaction.date}
+            </p>
+          </div>
         </div>
         <div className="bill-info-container">
           <div className="bill-info">
@@ -46,44 +51,48 @@ export default function SplitBillForm({ onClose, transaction }) {
           </div>
         </div>
 
-        <div className="participant-section">
-          <div className="participant-header">
-            <span>No</span>
-            <span>Bill Members</span>
-            <span>Amount Per Member</span>
-          </div>
-
-          {participants.map((p, i) => (
-            <div key={i} className="participant-row">
-              <span>{i + 1}</span>
-              <input
-                type="text"
-                placeholder="Enter name"
-                value={p.name}
-                onChange={(e) => handleChange(i, "name", e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Rp..."
-                value={p.amount}
-                onChange={(e) => handleChange(i, "amount", e.target.value)}
-              />
-              {i === participants.length - 1 && (
-                <button
-                  type="button"
-                  className="add-btn"
-                  onClick={handleAddParticipant}
-                >
-                  <Plus size={18} />
-                </button>
-              )}
+        <div className="participant-secition-container">
+          {" "}
+          <div className="participant-section">
+            <div className="participant-header">
+              <span>No</span>
+              <span>Bill Members</span>
+              <span>Amount Per Member</span>
             </div>
-          ))}
+            {participants.map((p, i) => (
+              <div key={i} className="participant-row">
+                <span>{i + 1}</span>
+                <input
+                  type="text"
+                  placeholder="Enter name"
+                  value={p.name}
+                  onChange={(e) => handleChange(i, "name", e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Rp..."
+                  value={p.amount}
+                  onChange={(e) => handleChange(i, "amount", e.target.value)}
+                />
+                {i === participants.length - 1 && (
+                  <button
+                    type="button"
+                    className="add-btn"
+                    onClick={handleAddParticipant}
+                  >
+                    <Plus size={18} />
+                  </button>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
 
-        <button className="add-participant-btn" onClick={handleSubmit}>
-          Add Participant
-        </button>
+        <div className="add-participant-btn-container">
+          <button className="add-participant-btn" onClick={handleSubmit}>
+            Add Participant
+          </button>
+        </div>
       </div>
     </div>
   );
