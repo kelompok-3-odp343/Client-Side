044eae34b2c737e0d93ff632dac305a19150937f
khairuddin-n <nastykhairuddin@gmail.com>
2025-10-16 08:40:41 +0700

    feat: split bill page initial commit

---
+++ src/pages/SplitBill.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../css/SplitBill.css";
+
+export default function SplitBill() {
+
+  return (
+    <div className="split-bill">
+      <Navbar />
+      <h1>Split Bill (WIP)</h1>
+    </div>
+  );
+}
