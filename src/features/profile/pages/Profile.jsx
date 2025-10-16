3352497bb3487ed6e200365bfcacbc6f89f99939
Ulion Pardede <ulionpardede28@gmail.com>
2025-10-17 00:10:51 +0700

    Penambahan service api pada profile

--- src/features/profile/pages/Profile.jsx
+++ src/features/profile/pages/Profile.jsx
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/profile.css";
 import Navbar from "../../../shared/components/Navbar";
+import { getUserProfile } from '../api/profileService';
 
 export default function Profile() {
+
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const response = await getUserProfile();
+      setProfile(response.data.profile);
+      setLoading(false);
+    };
+
+    getProfile();
+  }, []);
+
+  if (loading) return <p><center>Loading profile...</center></p>;
+
+  const fullName = [profile.first_name, profile.middle_name, profile.last_name]
+    .filter(Boolean)
+    .join(" ");
+
+  const formattedDOB = new Date(profile.dob).toLocaleDateString("id-ID", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+
   return (
     <div className="profile-page">
       <Navbar />
 
       <main className="profile-wrapper">
-        {/* LEFT SIDE */}
         <section className="profile-left">
           <div className="avatar">
             <i className="fas fa-user"></i>
           </div>
-          <h2 className="user-name">Ulion Pardede</h2>
+          <h2 className="user-name">{fullName}</h2>
 
           <div className="cif-card">
             <i className="fas fa-id-card"></i>
             <div>
               <p className="label">CIF</p>
-              <p className="value">9285711829</p>
+              <p className="value">{profile.cif}</p>
             </div>
           </div>
         </section>
@@ -33,7 +59,7 @@ export default function Profile() {
               <i className="fas fa-user"></i>
               <div>
                 <p className="label">Name</p>
-                <p className="value">Ulion Pardede</p>
+                <p className="value">{fullName}</p>
               </div>
             </div>
 
@@ -41,7 +67,7 @@ export default function Profile() {
               <i className="fas fa-calendar-alt"></i>
               <div>
                 <p className="label">Date of Birth</p>
-                <p className="value">28 August 1995</p>
+                <p className="value">{formattedDOB}</p>
               </div>
             </div>
 
@@ -49,7 +75,7 @@ export default function Profile() {
               <i className="fas fa-envelope"></i>
               <div>
                 <p className="label">Email Address</p>
-                <p className="value">ulion99pardede@gmail.com</p>
+                <p className="value">{profile.email_address}</p>
               </div>
             </div>
 
@@ -57,7 +83,7 @@ export default function Profile() {
               <i className="fas fa-phone"></i>
               <div>
                 <p className="label">Phone Number</p>
-                <p className="value">082376180082</p>
+                <p className="value">{profile.phone_number}</p>
               </div>
             </div>
           </div>
