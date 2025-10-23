21d9b94999c583083280a5897f57b464d22724d1
Ulion Pardede <ulionpardede28@gmail.com>
2025-10-23 22:48:41 +0700

    Perbaikan penamaan image

--- src/features/life-goals/constants/life-goals.meta.js
+++ src/features/life-goals/constants/life-goals.meta.js
@@ -1,9 +1,9 @@
-import educationIcon from "../../../assets/images/education.png";
-import vacationIcon from "../../../assets/images/vacation.png";
-import marriageIcon from "../../../assets/images/marriage.png";
-import homeIcon from "../../../assets/images/home.png";
-import gadgetIcon from "../../../assets/images/gadget.png";
-import vehicleIcon from "../../../assets/images/vehicle.png";
+import educationIcon from "../../../assets/images/Education.png";
+import vacationIcon from "../../../assets/images/Vacation.png";
+import marriageIcon from "../../../assets/images/Marriage.png";
+import homeIcon from "../../../assets/images/Home.png";
+import gadgetIcon from "../../../assets/images/Gadget.png";
+import vehicleIcon from "../../../assets/images/Vehicle.png";
 
 const LIFE_GOALS_META = {
   education: {
