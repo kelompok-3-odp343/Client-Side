c5b27693714b9b8aa447c4c5246b2dd4b9b0c5b2
khairuddin-n <nastykhairuddin@gmail.com>
2025-10-23 10:41:41 +0700

    update logo navbar dan perbaiki skalanya

--- src/shared/components/Navbar.jsx
+++ src/shared/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/components/navbar.css";
 import NotificationPanel from "./NotificationPanel";
+import logo from "../../assets/images/wandoor-logo-2.png";
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -21,17 +22,12 @@ export default function Navbar() {
     return () => document.removeEventListener("pointerdown", handleOutside);
   }, [showNotif]);
 
-
   return (
     <>
       <nav className="navbar-container">
         <div className="navbar-content">
           <div className="navbar-left" onClick={() => navigate("/dashboard")}>
-            <i className="fas fa-door-open navbar-logo"></i>
-            <h1 className="navbar-title">
-              wand<span className="o1">o</span>
-              <span className="o2">o</span>r
-            </h1>
+            <img src={logo} alt="Wandoor Logo" className="navbar-logo-img" />
           </div>
 
           <div className="navbar-right">
@@ -40,8 +36,8 @@ export default function Navbar() {
               className="fas fa-bell nav-icon"
               onClick={(e) => {
                 e.stopPropagation();
-                e.nativeEvent.stopImmediatePropagation();   // <-- ADD THIS
-                setShowNotif(prev => !prev);
+                e.nativeEvent.stopImmediatePropagation();
+                setShowNotif((prev) => !prev);
               }}
             ></i>
 
@@ -53,12 +49,7 @@ export default function Navbar() {
         </div>
       </nav>
 
-      {showNotif && (
-        <>
-          {/* <div className="notif-overlay" /> */}
-          <NotificationPanel visible={showNotif} anchorRef={bellRef} />
-        </>
-      )}
+      {showNotif && <NotificationPanel visible={showNotif} anchorRef={bellRef} />}
     </>
   );
 }
