910b6e5383983f27016679c98aa0acd39b8b51bc
khairuddin-n <nastykhairuddin@gmail.com>
2025-10-26 12:14:37 +0700

    fix: update kodenya supaya lolos tes sonarqube

--- src/features/life-goals/components/LifeGoalsCard.jsx
+++ src/features/life-goals/components/LifeGoalsCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../styles/life-goals-card.css";
 
 export default function LifeGoalsCard({ goal, onClick }) {
   const accent = goal.color || "#71d9d0";
-  const percent = Math.round((goal.current / goal.target) * 100);
+  const percent =
+    goal.target && goal.target > 0
+      ? Math.round((goal.current / goal.target) * 100)
+      : 0;
 
   return (
     <article
       className="lg-subcard"
       onClick={onClick}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") onClick(e);
+      }}
       role="button"
       tabIndex={0}
       style={{
@@ -18,8 +25,12 @@ export default function LifeGoalsCard({ goal, onClick }) {
       <div className="lg-subcard-body">
         <div className="lg-subcard-top">
           <div>
-            <h4 className="lg-subcard-title">{goal.title || goal.lifegoalsTitle}</h4>
-            <p className="lg-subcard-subtitle">{goal.lifegoalsSubtitle}</p>
+            <h4 className="lg-subcard-title">
+              {goal.title || goal.lifegoalsTitle || "-"}
+            </h4>
+            <p className="lg-subcard-subtitle">
+              {goal.lifegoalsSubtitle || ""}
+            </p>
           </div>
         </div>
 
@@ -39,11 +50,27 @@ export default function LifeGoalsCard({ goal, onClick }) {
           </div>
 
           <div className="lg-progress-values">
-            <span>Rp{goal.current.toLocaleString("id-ID")}</span>
-            <span>Rp{goal.target.toLocaleString("id-ID")}</span>
+            <span>
+              Rp{Number(goal.current || 0).toLocaleString("id-ID")}
+            </span>
+            <span>
+              Rp{Number(goal.target || 0).toLocaleString("id-ID")}
+            </span>
           </div>
         </div>
       </div>
     </article>
   );
 }
+
+LifeGoalsCard.propTypes = {
+  goal: PropTypes.shape({
+    title: PropTypes.string,
+    lifegoalsTitle: PropTypes.string,
+    lifegoalsSubtitle: PropTypes.string,
+    color: PropTypes.string,
+    current: PropTypes.number,
+    target: PropTypes.number,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
