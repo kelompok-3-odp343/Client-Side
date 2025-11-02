d6a7d525860ccecdc2a899927eea443039cbe07d
khairuddin-n <nastykhairuddin@gmail.com>
2025-11-02 22:54:22 +0700

    revamp savings, baru mulai jadi masih perlu banyak perbaikan

--- src/features/saving/api/savings.api.js
+++ src/features/saving/api/savings.api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
 export const getSavingsData = async (userId) => {
     try {
         const response = await api.get(`/api/savings/${userId}`);
@@ -18,32 +20,37 @@ export const getSavingsData = async (userId) => {
         const dummyResponse = {
             status: true,
             message: "Dummy data sementara",
-            data: [{
-                account_id: "ACCT_9876543210",
-                title: "Savings - TAPLUS PEGAWAI BNI",
-                total_balance: 500000000,
-                items: [{
-                    item_id: "ACCT_9876543210",
-                    account_number: "1234567899",
-                    account_name: "TAPLUS PEGAWAI BNI",
-                    effective_balance: 500000000,
-                    is_main_account: false,
-                    account_status: "BUKA",
-                },],
-            },
-            {
-                account_id: "ACCT_12345678910",
-                title: "Savings - TAPLUS BISNIS",
-                total_balance: 12000000000,
-                items: [{
-                    item_id: "ACCT_12345678910",
-                    account_number: "12345678910",
-                    account_name: "TAPLUS BISNIS",
-                    effective_balance: 12000000000,
-                    is_main_account: true,
-                    account_status: "BUKA",
-                },],
-            },
+            data: [
+                {
+                    account_id: "ACCT_9876543210",
+                    title: "Savings - TAPLUS PEGAWAI BNI",
+                    total_balance: 500000000,
+                    items: [
+                        {
+                            item_id: "ACCT_9876543210",
+                            account_number: "1234567899",
+                            account_name: "TAPLUS PEGAWAI BNI",
+                            effective_balance: 500000000,
+                            is_main_account: false,
+                            account_status: "BUKA",
+                        },
+                    ],
+                },
+                {
+                    account_id: "ACCT_12345678910",
+                    title: "Savings - TAPLUS BISNIS",
+                    total_balance: 12000000000,
+                    items: [
+                        {
+                            item_id: "ACCT_12345678910",
+                            account_number: "12345678910",
+                            account_name: "TAPLUS BISNIS",
+                            effective_balance: 12000000000,
+                            is_main_account: true,
+                            account_status: "BUKA",
+                        },
+                    ],
+                },
             ],
         };
 
