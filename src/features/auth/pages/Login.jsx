3b0f02d13f9bff3a535febb33591d40617b32f28
Ulion Pardede <ulionpardede28@gmail.com>
2025-11-17 13:33:33 +0700

    Update Login untuk kebutuhan VM

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -4,6 +4,7 @@ import "../styles/auth.css";
 import "../styles/auth-login.css";
 import logo from "../../../assets/images/wandoor-logo-2.png";
 import { postAuthLogin } from "../api/authService";
+import { decodeJwtToken } from "../api/jwtHelper";
 
 export default function Login() {
 	const [username, setUsername] = useState("");
@@ -39,11 +40,17 @@ export default function Login() {
 			return;
 		}
 
-		const { role, sessionIdOrToken, token, message: apiMsg } = respLogin.data;
+		const token = respLogin.data.sessionIdOrToken;
 
-		sessionStorage.setItem("sessionID", sessionIdOrToken || "NO_SESSION");
-		sessionStorage.setItem("token", token || "DUMMY_TOKEN");
-		sessionStorage.setItem("role", role || "user");
+		let decoded = {};
+		try {
+			decoded = decodeJwtToken(token);
+		} catch (err) {
+			console.error("JWT decode error:", err);
+		}
+
+		sessionStorage.setItem("token", token);
+		sessionStorage.setItem("role", decoded.role || "NASABAH");
 
 		setMessage(apiMsg || "Login success");
 		setModalType("success");
@@ -51,11 +58,21 @@ export default function Login() {
 
 		setTimeout(() => {
 			setShowModal(false);
-			if (role === "admin") {
+
+			const roleUpper = (decoded.role || "").toUpperCase();
+			const ADMIN_ROLES = ["ADMIN", "MAKER", "CHECKER", "APPROVAL"];
+
+			if (ADMIN_ROLES.includes(roleUpper)) {
+				sessionStorage.setItem('npp', decoded.npp || "ADM001");
 				navigate("/admin/home");
-			} else {
-				navigate("/otpLogin");
+				return;
 			}
+
+			if (roleUpper === "NASABAH") {
+				navigate("/otpLogin");
+				return;
+			}
+			navigate("/otpLogin");
 		}, 800);
 	};
 
